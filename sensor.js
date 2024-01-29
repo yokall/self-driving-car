0390ea9b37f49db8597e1a581a25d9c1867b55cd
Colin Campbell <colincampbell321123@hotmail.com>
2024-01-29 21:21:45 +0000

    Add collission detection

--- sensor.js
+++ sensor.js
@@ -2,7 +2,7 @@ class Sensor {
     constructor(car) {
         this.car = car;
 
-        this.rayCount = 3;
+        this.rayCount = 5;
         this.rayLength = 150;
         this.raySpread = Math.PI / 4;
 
