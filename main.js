fd54edf25a13d4ada30b9f7a652e55984dae94e7
Colin Campbell <colincampbell321123@hotmail.com>
2024-02-18 16:02:07 +0000

    Attempt to automatically pick best car and re-run
    Needs a better fitness function as it rewards stalker cars following behind traffic

--- main.js
+++ main.js
@@ -8,31 +8,20 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
-const cars = generateCars(100);
+let cars = setupCars();
+let traffic = setupTraffic();
+
 let bestCar = cars[0];
-if (localStorage.getItem("bestBrain")) {
-    for (let i = 0; i < cars.length; i++) {
-        cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
 
-        if (i != 0) {
-            NeuralNetwork.mutate(cars[i].brain, 0.3);
-        }
-    }
-}
-
-const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(2), -200, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(0), -600, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(1), -1000, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(0), -850, 30, 50, "DUMMY", 1.5),
-    new Car(road.getLaneCenter(2), -800, 30, 50, "DUMMY", 1.5),
-];
+let generation = 0;
+discardBrain();
 
 animate();
+
+function reset() {
+    cars = setupCars();
+    traffic = setupTraffic();
+}
 
 function saveBrain() {
     localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
@@ -42,10 +31,41 @@ function discardBrain() {
     localStorage.removeItem("bestBrain");
 }
 
+function setupCars() {
+    let cars = generateCars(100);
+    if (localStorage.getItem("bestBrain")) {
+        for (let i = 0; i < cars.length; i++) {
+            cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+
+            if (i != 0) {
+                NeuralNetwork.mutate(cars[i].brain, 0.3);
+            }
+        }
+    }
+
+    return cars;
+}
+
+function setupTraffic() {
+    let traffic = [
+        new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(2), -200, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(0), -600, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(1), -1000, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(0), -850, 30, 50, "DUMMY", 1.5),
+        new Car(road.getLaneCenter(2), -800, 30, 50, "DUMMY", 1.5),
+    ];
+
+    return traffic;
+}
+
 function generateCars(n) {
     const cars = [];
     for (let i = 1; i <= n; i++) {
-        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+        cars.push(new Car(road.getLaneCenter(1), 0, 30, 50, "AI"));
     }
 
     return cars;
@@ -59,6 +79,8 @@ function animate(time) {
     cars.forEach(car => {
         car.update(road.borders, traffic);
     });
+
+    // need a better fitness function that doesn't reward stalker cars
     bestCar = cars.find(car =>
         car.y == Math.min(
             ...cars.map(c => c.y)
@@ -87,6 +109,14 @@ function animate(time) {
 
     networkCtx.lineDashOffset = -time / 50;
     Visualizer.drawNetwork(networkCtx, bestCar.brain);
+
+    const newGeneration = Math.floor(time / 15000);
+
+    if (generation != newGeneration) {
+        saveBrain();
+        reset();
+        generation = newGeneration;
+    }
 
     requestAnimationFrame(animate);
 }
